f04bc227ac3eb59d14ca93da1187bf44c04d691e
Jungtaek Lim <kabhwan@gmail.com>
2016-02-01 12:48:43 +0900

    Make groovy.lang.Script transient

--- src/main/java/net/heartsavior/GroovyScriptNativeShell.java
+++ src/main/java/net/heartsavior/GroovyScriptNativeShell.java
@@ -2,7 +2,6 @@ package net.heartsavior;
 
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
-import groovy.lang.Script;
 
 import javax.script.ScriptException;
 import java.util.Map;
@@ -10,17 +9,15 @@ import java.util.Map;
 
 public class GroovyScriptNativeShell<O> implements GroovyScript<O> {
     private final String expression;
-    private final Script parsedScript;
 
-    private GroovyShell groovyShell;
+    private transient groovy.lang.Script parsedScript;
 
     public GroovyScriptNativeShell(String expression) {
         this.expression = expression;
-        this.groovyShell = new GroovyShell();
-        this.parsedScript = groovyShell.parse(this.expression);
     }
 
     public O evaluate(Map<String, Object> params) throws ScriptException {
+        groovy.lang.Script parsedScript = getParsedScript();
         O evaluatedResult = null;
 
         if (params != null) {
@@ -34,5 +31,13 @@ public class GroovyScriptNativeShell<O> implements GroovyScript<O> {
         }
 
         return evaluatedResult;
+    }
+
+    private groovy.lang.Script getParsedScript() {
+        if (parsedScript == null) {
+            GroovyShell groovyShell = new GroovyShell();
+            parsedScript = groovyShell.parse(expression);
+        }
+        return parsedScript;
     }
 }
